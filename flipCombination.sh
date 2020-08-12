a5b353f75df668619b303eab1c0c32860ea02cb5
naincyrani06 <naincyrani06it@gmail.com>
2020-08-12 14:29:53 +0530

    Displaying Head Or Tails

--- flipCombination.sh
+++ flipCombination.sh
@@ -1,2 +1,15 @@
 #!/bin/bash -x
-echo "Welcome To Flip Coin Problem"
+   #echo "Welcome To Flip Coin Problem"
+
+flipCheck=$((RANDOM%2))
+
+isHead=1
+
+echo "Flipping Coin Value: "$flipCheck
+
+if [ $flipCheck -eq $isHead ]
+then
+	echo " Head is Winner"
+else
+	echo " Tail is Winner"
+fi
