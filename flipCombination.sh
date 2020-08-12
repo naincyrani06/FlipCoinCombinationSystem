921d6c48ebf16f7d2700639de9ac9ab4ff6ba03f
naincyrani06 <naincyrani06it@gmail.com>
2020-08-12 14:37:01 +0530

    Storing Singlet Combination in Dictionary And Displaying Percentage

--- flipCombination.sh
+++ flipCombination.sh
@@ -1,15 +1,32 @@
 #!/bin/bash -x
    #echo "Welcome To Flip Coin Problem"
 
-flipCheck=$((RANDOM%2))
+declare -A flipCheck
+declare -A flipCheckSinglet
+declare -A percentSingletDictionary
 
 isHead=1
+for((i=1;i<=70;i++))
+do
+	flipCheck=$((RANDOM%2))
+	if [ $flipCheck -eq $isHead ]
+	then
+		countHead=$((countHead + 1))
+	else
+                countTail=$((countTail + 1))
+	fi
+done
 
-echo "Flipping Coin Value: "$flipCheck
+flipCheck[Head]=$countHead
+flipCheckSinglet[Head]=$countHead
 
-if [ $flipCheck -eq $isHead ]
-then
-	echo " Head is Winner"
-else
-	echo " Tail is Winner"
-fi
+flipCheck[Tail]=$countTail
+flipCheckSinglet[Tail]=$countTail
+echo "Head is :${flipCheck[Head]}"
+echo "Head is :${flipCheckSinglet[Head]}"
+echo "Tail :${flipCheck[Tail]}"
+echo "Tail :${flipCheckSinglet[Tail]}"
+echo "Percentage of getting Head : "`awk "BEGIN {print ($countHead/20)*100 }"`
+echo "Percentage of getting Tail : "`awk "BEGIN {print ($countTail/20)*100 }"`
+percentSingletDictionary[Head]=`awk "BEGIN {print ($countHead/20)*100 }"`
+percentSingletDictionary[Tail]=`awk "BEGIN {print ($countTail/20)*100 }"`
